d1616527f73c2be74277a829caa385192127118f
sofinms <sofinms@gmail.com>
2018-07-21 09:24:35 +0400

    add simple_view mode in model show

--- bin/model_show.rb
+++ bin/model_show.rb
@@ -4,7 +4,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'model_file'
 
 model_path = ARGV.first
+view_type = ARGV[1]
 abort "USAGE: #{$0} <filepath>" unless model_path
 
-model = ModelFile.parse_file(model_path)
+model = ModelFile.parse_file(model_path, view_type)
 model.print
