32ae1e79b1176e15bf0400f140cb71222d9a1470
Ilia Komarov <nexen505@yandex.ru>
2018-07-22 13:39:07 +0400

    Fixed missing 'require'.

--- lib/command.rb
+++ lib/command.rb
@@ -1,16 +1,17 @@
-require 'command/base'
-require 'command/fill'
-require 'command/fission'
-require 'command/flip'
-require 'command/fusionp'
-require 'command/fusions'
-require 'command/halt'
-require 'command/lmove'
-require 'command/smove'
-require 'command/wait'
+require "command/base"
+require "command/fill"
+require "command/fission"
+require "command/flip"
+require "command/fusionp"
+require "command/fusions"
+require "command/halt"
+require "command/lmove"
+require "command/smove"
+require "command/wait"
+require "command/void"
 
 module Command
-	def self.print_commands commands
-		puts commands.join("\n")
-	end
+  def self.print_commands(commands)
+    puts commands.join("\n")
+  end
 end
