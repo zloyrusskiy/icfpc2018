1934ea13be646aa608eed694e1a19173d63b2543
Ilia Komarov <nexen505@yandex.ru>
2018-07-22 14:34:44 +0400

    Script for trace calculation. Reverted reverse.

--- lib/model_file.rb
+++ lib/model_file.rb
@@ -1,31 +1,31 @@
-require 'voxel'
+require "voxel"
 
 Model = Struct.new(:dimensions, :points) do
-  def print view_type = nil
-    if view_type && (view_type == 'simple_view')
+  def print(view_type = nil)
+    if view_type && (view_type == "simple_view")
       puts dimensions.to_s
       points.each do |layer|
-        layer_header = ''
-        layer_output = ''
+        layer_header = ""
+        layer_output = ""
 
         layer.each do |row|
           layer_output += row.join + "\n"
         end
 
-        if layer_output.include? '1'
-          puts layer_header + '' + layer_output + ''
+        if layer_output.include? "1"
+          puts layer_header + "" + layer_output + ""
         end
       end
     else
       points.each.with_index do |layer, y|
         layer_header = "  -- ##{y} --"
-        layer_output = ''
+        layer_output = ""
 
         layer.each do |row|
           layer_output += row.join + "\n"
         end
 
-        if layer_output.include? '1'
+        if layer_output.include? "1"
           puts layer_header + "\n" + layer_output + "\n"
         end
       end
@@ -43,12 +43,12 @@ Model = Struct.new(:dimensions, :points) do
     points.each.with_index do |layer, y|
       layer.each.with_index do |row, z|
         vxls.concat(
-            row
-                .map
-                .with_index
-                .map do |cell, x|
-              Voxel.new(x, y, z, Voxel.full?(cell))
-            end
+          row
+            .map
+            .with_index
+            .map do |cell, x|
+            Voxel.new(x, y, z, Voxel.full?(cell))
+          end
         )
       end
     end
@@ -56,29 +56,28 @@ Model = Struct.new(:dimensions, :points) do
   end
 
   def filled_voxels
-    voxels.select {|voxel| voxel.full}
+    voxels.select { |voxel| voxel.full }
   end
-
 end
 
 module ModelFile
   def self.parse_file(filepath)
     model_data = File.binread(filepath)
-    dimensions, fill_bits = model_data.unpack('Cb*')
+    dimensions, fill_bits = model_data.unpack("Cb*")
     total_fill_bits = dimensions ** 3
 
     excess_bits_qty = fill_bits.size - total_fill_bits
 
     points = fill_bits
-                 .chars
-                 .drop(excess_bits_qty)
-                 .map(&:to_i)
-                 .each_slice(dimensions)
-                 .each_slice(dimensions)
-                 .to_a
-                 .transpose
-                 .map(&:transpose)
-                 # .map(&:reverse) # z вверху
+      .chars
+      .drop(excess_bits_qty)
+      .map(&:to_i)
+      .each_slice(dimensions)
+      .each_slice(dimensions)
+      .to_a
+      .transpose
+      .map(&:transpose)
+      .map(&:reverse)
 
     Model.new(dimensions, points)
   end
